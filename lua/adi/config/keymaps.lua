a19039f087da7510e972dca21eab7500ea0fac52
adi-075 <adityamhambrey323@gmail.com>
2023-03-15 18:12:25 +0530

    Update Keymaps

--- lua/adi/config/keymaps.lua
+++ lua/adi/config/keymaps.lua
@@ -4,5 +4,8 @@ vim.keymap.set('n', '<leader>d', '<cmd>Dashboard<cr>')
 vim.keymap.set('n', '<leader>ps', '<cmd>PackerSync<cr>')
 vim.keymap.set('n', '<leader>pi', '<cmd>PackerInstall<cr>')
 vim.keymap.set('n', '<leader>pc', '<cmd>PackerCompile<cr>')
+vim.keymap.set('n', '<leader>v', '<cmd>vnew<cr>')
+vim.keymap.set('n', '<leader>h', '<cmd>split<cr>')
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>')
 
 
