48c52b76ab136168de536a671d0c802fd7e2885a
adi-075 <adityamhambrey323@gmail.com>
2023-03-15 19:51:32 +0530

    Update Keymaps

--- lua/adi/config/keymaps.lua
+++ lua/adi/config/keymaps.lua
@@ -1,11 +1,18 @@
--- keymap
+-- Keymaps
+
+-- File Operations
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
-vim.keymap.set('n', '<leader>d', '<cmd>Dashboard<cr>')
-vim.keymap.set('n', '<leader>ps', '<cmd>PackerSync<cr>')
-vim.keymap.set('n', '<leader>pi', '<cmd>PackerInstall<cr>')
-vim.keymap.set('n', '<leader>pc', '<cmd>PackerCompile<cr>')
 vim.keymap.set('n', '<leader>v', '<cmd>vnew<cr>')
 vim.keymap.set('n', '<leader>h', '<cmd>split<cr>')
 vim.keymap.set('n', '<leader>q', '<cmd>q<cr>')
 
+-- UI
+vim.keymap.set('n', '<leader>d', '<cmd>Dashboard<cr>')
+vim.keymap.set('n', '<leader>e', '<cmd>NeoTreeFocusToggle<cr>', { silent = true })
 
+-- Package Management
+vim.keymap.set('n', '<leader>p', '<cmd>Lazy<cr>')
+
+-- LSP
+vim.keymap.set('n', '<leader>li', '<cmd>LspInstall<cr>')
+vim.keymap.set('n', '<leader>lf', '<cmd>LspZeroFormat<cr>')
