ed883f5f3a587e35fb4902b65df108115cbd13a9
adi-075 <adityamhambrey323@gmail.com>
2023-03-26 15:56:02 +0530

    Update Nvim-Cmp

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -3,31 +3,15 @@ if not cmp_status_ok then
   return
 end
 
-
 local cmp_action = require('lsp-zero').cmp_action()
-
 cmp.setup {
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   view = {
     entries = "custom" -- can be "custom", "wildmenu" or "native"
   },
-
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
-
-  window = {
-    documentation = cmp.config.window.bordered(),
-  },
-
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip', keyword_length = 2 },
-    { name = 'path' },
-    { name = 'buffer',  keyword_length = 3 },
-  },
-
   mapping = {
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
@@ -35,5 +19,15 @@ cmp.setup {
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ['<C-e>'] = cmp.mapping.abort(),
   },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip', keyword_length = 2 },
+    { name = 'path' },
+    { name = 'buffer',  keyword_length = 3 },
+  },
 }
-
