f42862e1c5f8026cf9f20f0d10fcf8042d8b2ebe
adi-075 <adityamhambrey323@gmail.com>
2023-04-02 10:02:58 +0530

    Update Lazy Packages

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -28,6 +28,22 @@ require("lazy").setup({
     }
   },
 
+  -- Color highlights
+  {
+    'brenoprata10/nvim-highlight-colors',
+    config = function()
+      require "nvim-highlight-colors".setup({
+        render = 'background', -- or 'foreground' or 'first_column'
+        enable_named_colors = true,
+        enable_tailwind = true
+      })
+    end,
+  },
+
+  {
+    "yorik1984/lualine-theme.nvim",
+  },
+
   -- Indents
   {
     'lukas-reineke/indent-blankline.nvim',
@@ -35,15 +51,34 @@ require("lazy").setup({
       -- Rainbow Brackets
       { "ChristianChiarulli/nvim-ts-rainbow" },
       { 'nvim-treesitter/nvim-treesitter' },
+      {
+        'folke/trouble.nvim',
+        dependencies = {
+          "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+          require("trouble").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+          }
+        end
+      },
     },
     config = function()
+      vim.opt.termguicolors = true
+      vim.cmd [[highlight IndentBlanklineIndent1 guifg=#353B55 gui=nocombine]]
       vim.opt.list = true
-      -- vim.opt.listchars:append "space:⋅"
-      -- vim.opt.listchars:append "eol:↴"
       require("indent_blankline").setup({
-        show_end_of_line = true,
+        -- show_end_of_line = true,
         space_char_blankline = " ",
-        show_current_context_start = true,
+        use_treesitter = true,
+        use_treesitter_scope = true,
+        char = "▎",
+        --[[ char_highlight_list = {
+          "IndentBlanklineIndent1",
+        }, ]]
+        -- show_current_context_start = true,
       })
     end,
   },
@@ -55,9 +90,19 @@ require("lazy").setup({
     dependencies = {
       { 'nvim-tree/nvim-web-devicons' },
     },
-    config = function ()
-      require("bufferline").setup{}
-    end
+    config = function()
+      require("bufferline").setup({
+        options = {
+          mode = "buffers", -- set to "tabs" to only show tabpages instead
+          indicator = {
+            icon = '▎',   -- this should be omitted if indicator style is not 'icon'
+            style = 'icon',
+          },
+          diagnostics = "nvim_lsp",
+          color_icons = true, -- whether or not to add the filetype icon highlights
+        },
+      })
+    end,
   },
 
   {
@@ -106,14 +151,14 @@ require("lazy").setup({
   "nvim-treesitter/nvim-treesitter",
 
   -- Colorschemes
-  {
+  --[[ {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     config = function()
       require("tokyonight").setup({
         style = "night",                  -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        transparent = false,              -- Enable this to disable setting the background color
+        transparent = true,               -- Enable this to disable setting the background color
         terminal_colors = true,           -- Configure the colors used when opening a `:terminal` in Neovim
         sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
         hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
@@ -123,9 +168,94 @@ require("lazy").setup({
     end,
   },
 
+  'christianchiarulli/nvcode-color-schemes.vim', ]]
+
   {
-    'rose-pine/neovim',
-    name = 'rose-pine'
+    'navarasu/onedark.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = 'darker',
+      })
+      vim.cmd('colorscheme onedark')
+    end,
+  },
+
+  {
+  },
+
+  {
+    'folke/todo-comments.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+    },
+    config = function()
+      require("todo-comments").setup({
+        signs = true,      -- show icons in the signs column
+        sign_priority = 8, -- sign priority
+        -- keywords recognized as todo comments
+        keywords = {
+          FIX = {
+            icon = " ",                              -- icon used for the sign, and in search results
+            color = "error",                            -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = { icon = " ", color = "info" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+          TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        },
+        gui_style = {
+          fg = "NONE",         -- The gui style to use for the fg highlight group.
+          bg = "BOLD",         -- The gui style to use for the bg highlight group.
+        },
+        merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+        -- highlighting of the line containing the todo comment
+        -- * before: highlights before the keyword (typically comment characters)
+        -- * keyword: highlights of the keyword
+        -- * after: highlights after the keyword (todo text)
+        highlight = {
+          multiline = true,                -- enable multine todo comments
+          multiline_pattern = "^.",        -- lua pattern to match the next multiline from the start of the matched keyword
+          multiline_context = 10,          -- extra lines that will be re-evaluated when changing a line
+          before = "",                     -- "fg" or "bg" or empty
+          keyword = "wide",                -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+          after = "fg",                    -- "fg" or "bg" or empty
+          pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+          comments_only = true,            -- uses treesitter to match keywords in comments only
+          max_line_len = 400,              -- ignore lines longer than this
+          exclude = {},                    -- list of file types to exclude highlighting
+        },
+        -- list of named colors where we try to extract the guifg from the
+        -- list of highlight groups or use the hex color if hl not found as a fallback
+        colors = {
+          error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+          warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+          info = { "DiagnosticInfo", "#2563EB" },
+          hint = { "DiagnosticHint", "#10B981" },
+          default = { "Identifier", "#7C3AED" },
+          test = { "Identifier", "#FF00FF" }
+        },
+        search = {
+          command = "rg",
+          args = {
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+          },
+          -- regex that will be used to match keywords.
+          -- don't replace the (KEYWORDS) placeholder
+          pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+          -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+        },
+      })
+    end,
   },
 
   "nvim-lua/plenary.nvim",
@@ -207,35 +337,19 @@ require("lazy").setup({
       "MunifTanjim/nui.nvim",
     },
     config = function()
-      require("neo-tree").setup({
-        popup_border_style = "rounded",
-        enable_git_status = true,
-        enable_diagnostics = true,
-        icon = {
-          folder_closed = "",
-          folder_open = "",
-          folder_empty = "ﰊ",
-          -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
-          -- then these will never be used.
-          default = "*",
-          highlight = "NeoTreeFileIcon"
-        },
-        git_status = {
-          symbols = {
-            -- Change type
-            added     = "",  -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified  = "",  -- or "", but this is redundant info if you use git_status_colors on the name
-            deleted   = "✖", -- this can only be used in the git_status source
-            renamed   = "", -- this can only be used in the git_status source
-            -- Status type
-            untracked = "",
-            ignored   = "",
-            unstaged  = "",
-            staged    = "",
-            conflict  = "",
-          }
-        },
-      })
+      -- Unless you are still migrating, remove the deprecated commands from v1.x
+      vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+      -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+      vim.fn.sign_define("DiagnosticSignError",
+        { text = " ", texthl = "DiagnosticSignError" })
+      vim.fn.sign_define("DiagnosticSignWarn",
+        { text = " ", texthl = "DiagnosticSignWarn" })
+      vim.fn.sign_define("DiagnosticSignInfo",
+        { text = " ", texthl = "DiagnosticSignInfo" })
+      vim.fn.sign_define("DiagnosticSignHint",
+        { text = "", texthl = "DiagnosticSignHint" })
+      require("neo-tree").setup {}
     end,
   },
 
